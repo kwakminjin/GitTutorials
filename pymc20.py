5214b21b585852b15ea285387c4a002835260022
kwakminjin <mjk6570@naver.com>
2022-02-09 23:54:58 +0900

    파이썬 머신러닝 공부_2022.02.09

---
+++ pymc20.py
@@ -0,0 +1,415 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+import pandas as pd
+import datetime as dt
+import numpy as np
+import datetime as dt
+import matplotlib.pyplot as plt
+import seaborn as sns
+from  scipy.stats import skew, kurtosis, shapiro
+
+
+# In[4]:
+
+
+path = "c:\spain_energy_market.csv"
+data = pd.read_csv(path, sep=",", parse_dates=["datetime"])
+#parse_dates는 날짜를 datetime 형태로 변환할지 여부인데, True라고 했으니 해당 컬럼에 있는 데이터는 날짜 형태가 된다.
+data = data[data["name"]=="Demanda programada PBF total"]#.set_index("datetime")
+data["date"] = data["datetime"].dt.date ## YYYY-MM-DD(문자) 반환형태
+data.set_index("date", inplace=True) #date열을 인덱스로 사용
+data = data[["value"]] #value값만 추출
+data = data.asfreq("D") #asfreq는 Datetime Index를 원하는 주기로 나누어주는 메서드 입니다.
+#이러한 방식을 리샘플링(resample)이라고 합니다. D = day
+data = data.rename(columns={"value": "energy"})
+data.info()          
+
+
+# In[21]:
+
+
+data[:5]
+
+
+# In[5]:
+
+
+data.plot(title="Energy Demand")
+plt.ylabel("MWh")
+plt.show()
+
+
+# In[23]:
+
+
+len(pd.date_range(start="2014-01-01", end="2018-12-31"))
+
+
+# In[6]:
+
+
+data["year"] = data.index.year
+data["qtr"] = data.index.quarter #분기(숫자)
+data["mon"] = data.index.month
+data["week"] = data.index.week
+data["day"] = data.index.weekday #정수로 요일을 반환합니다. 월요일은 0이고 일요일은 6
+data["ix"] = range(0,len(data))
+data[["movave_7", "movstd_7"]] = data.energy.rolling(7).agg([np.mean, np.std]) #7일 이동평균값계산 rolling : 이동평균계산
+data[["movave_30", "movstd_30"]] = data.energy.rolling(30).agg([np.mean, np.std]) #agg : 다중집계작업
+data[["movave_90", "movstd_90"]] = data.energy.rolling(90).agg([np.mean, np.std])
+data[["movave_365", "movstd_365"]] = data.energy.rolling(365).agg([np.mean, np.std])
+
+plt.figure(figsize=(20,16))
+data[["energy", "movave_7"]].plot(title="Daily Energy Demand in Spain (MWh)")
+plt.ylabel("(MWh)")
+plt.show()
+
+
+# In[7]:
+
+
+mean = np.mean(data.energy.values)
+std = np.std(data.energy.values)
+skew = skew(data.energy.values) #왜도
+ex_kurt = kurtosis(data.energy) #첨도
+print("Skewness: {} \nKurtosis: {}".format(skew, ex_kurt+3))
+
+
+# In[8]:
+
+
+def shapiro_test(data, alpha=0.05):
+    stat, pval = shapiro(data) #검정통계량, pvalue
+    print("H0: Data was drawn from a Normal Ditribution")
+    if (pval<alpha):
+        print("pval {} is lower than significance level: {}, therefore null hypothesis is rejected".format(pval, alpha))
+    else:
+        print("pval {} is higher than significance level: {}, therefore null hypothesis cannot be rejected".format(pval, alpha))
+        
+shapiro_test(data.energy, alpha=0.05)
+
+
+# In[9]:
+
+
+sns.distplot(data.energy)
+plt.title("Target Analysis")
+plt.xticks(rotation=45)
+plt.xlabel("(MWh)")
+plt.axvline(x=mean, color='r', linestyle='-', label="\mu: {0:.2f}%".format(mean))
+plt.axvline(x=mean+2*std, color='orange', linestyle='-')
+plt.axvline(x=mean-2*std, color='orange', linestyle='-')
+plt.show()
+
+
+# In[10]:
+
+
+# Insert the rolling quantiles to the monthly returns
+data_rolling = data.energy.rolling(window=90)
+data['q10'] = data_rolling.quantile(0.1).to_frame("q10")
+data['q50'] = data_rolling.quantile(0.5).to_frame("q50")
+data['q90'] = data_rolling.quantile(0.9).to_frame("q90")
+
+data[["q10", "q50", "q90"]].plot(title="Volatility Analysis: 90-rolling percentiles")
+plt.ylabel("(MWh)")
+plt.show()
+
+
+# In[11]:
+
+
+data.groupby("qtr")["energy"].std().divide(data.groupby("qtr")["energy"].mean()).plot(kind="bar")
+#divide()로 나누어줌
+plt.title("Coefficient of Variation (CV) by qtr")
+plt.show()
+
+
+# In[12]:
+
+
+data.groupby("mon")["energy"].std().divide(data.groupby("mon")["energy"].mean()).plot(kind="bar")
+plt.title("Coefficient of Variation (CV) by month")
+plt.show()
+
+
+# In[13]:
+
+
+data[["movstd_30", "movstd_365"]].plot(title="Heteroscedasticity analysis")
+plt.ylabel("(MWh)")
+plt.show()
+
+
+# In[14]:
+
+
+data[["movave_30", "movave_90"]].plot(title="Seasonal Analysis: Moving Averages")
+plt.ylabel("(MWh)")
+plt.show()
+
+
+# In[15]:
+
+
+sns.boxplot(data=data, x="qtr", y="energy")
+plt.title("Seasonality analysis: Distribution over quaters")
+plt.ylabel("(MWh)")
+plt.show()
+
+
+# In[16]:
+
+
+sns.boxplot(data=data, x="day", y="energy")
+plt.title("Seasonality analysis: Distribution over weekdays")
+plt.ylabel("(MWh)")
+plt.show()
+
+
+# In[18]:
+
+
+data_mon = data.energy.resample("M").agg(sum).to_frame("energy")
+#datetime 객체인 인덱스를 resample('M') 메서드가 월별로 그룹핑합니다.
+data_mon["ix"] = range(0, len(data_mon))
+data_mon[:5]
+
+
+# In[19]:
+
+
+sns.regplot(data=data_mon,x="ix", y="energy")
+plt.title("Trend analysis: Regression")
+plt.ylabel("(MWh)")
+plt.xlabel("")
+plt.show()
+
+
+# In[20]:
+
+
+sns.boxplot(data=data["2014":"2017"], x="year", y="energy")
+plt.title("Trend Analysis: Annual Box-plot Distribution")
+plt.ylabel("(MWh)")
+plt.show()
+
+
+# In[21]:
+
+
+data["target"] = data.energy.add(-mean).div(std) #div : 나눗셈
+sns.distplot(data["target"])
+plt.show()
+
+
+# In[22]:
+
+
+features = []
+corr_features=[]
+targets = []
+tau = 30 #forecasting periods
+
+for t in range(1, tau+1):
+    data["target_t" + str(t)] = data.target.shift(-t) #target은 무엇?
+    targets.append("target_t" + str(t))
+    
+for t in range(1,31):
+    data["feat_ar" + str(t)] = data.target.shift(t)
+    #data["feat_ar" + str(t) + "_lag1y"] = data.target.shift(350)
+    features.append("feat_ar" + str(t))
+    #corr_features.append("feat_ar" + str(t))
+    #features.append("feat_ar" + str(t) + "_lag1y")
+        
+    
+for t in [7, 14, 30]:
+    data[["feat_movave" + str(t), "feat_movstd" + str(t), "feat_movmin" + str(t) ,"feat_movmax" + str(t)]] = data.energy.rolling(t).agg([np.mean, np.std, np.max, np.min])
+    features.append("feat_movave" + str(t))
+    #corr_features.append("feat_movave" + str(t))
+    features.append("feat_movstd" + str(t))
+    features.append("feat_movmin" + str(t))
+    features.append("feat_movmax" + str(t))
+    
+months = pd.get_dummies(data.mon,
+                              prefix="mon",
+                              drop_first=True)
+months.index = data.index
+data = pd.concat([data, months], axis=1) #가로로 합치기
+
+days = pd.get_dummies(data.day,
+                              prefix="day",
+                              drop_first=True)
+days.index = data.index
+data = pd.concat([data, days], axis=1)
+
+
+features = features + months.columns.values.tolist() + days.columns.values.tolist()
+
+
+# In[23]:
+
+
+corr_features = ["feat_ar1", "feat_ar2", "feat_ar3", "feat_ar4",
+                 "feat_ar5", "feat_ar6", "feat_ar7", "feat_movave7",
+                 "feat_movave14", "feat_movave30"]
+
+
+# In[24]:
+
+
+# Calculate correlation matrix
+corr = data[["target_t1"] + corr_features].corr()
+
+top5_mostCorrFeats = corr["target_t1"].apply(abs).sort_values(ascending=False).index.values[:6]
+
+
+# Plot heatmap of correlation matrix
+sns.heatmap(corr, annot=True)
+plt.title("Pearson Correlation with 1 period target")
+plt.yticks(rotation=0); plt.xticks(rotation=90)  # fix ticklabel directions
+plt.tight_layout()  # fits plot area to the plot, "tightly"
+plt.show()  # show the plot
+
+
+# In[25]:
+
+
+sns.pairplot(data=data[top5_mostCorrFeats].dropna(), kind="reg")
+plt.title("Most important features Matrix Scatter Plot")
+plt.show()
+
+
+# In[26]:
+
+
+data_feateng = data[features + targets].dropna()
+nobs= len(data_feateng)
+print("Number of observations: ", nobs)
+
+
+# In[33]:
+
+
+X_train = data_feateng.loc["2014":"2017"][features]
+y_train = data_feateng.loc["2014":"2017"][targets]
+
+X_test = data_feateng.loc["2018"][features]
+y_test = data_feateng.loc["2018"][targets]
+
+n, k = X_train.shape
+print("Total number of observations: ", nobs)
+print("Train: {}{}, \nTest: {}{}".format(X_train.shape, y_train.shape,
+                                              X_test.shape, y_test.shape))
+
+plt.plot(y_train.index, y_train.target_t1.values, label="train")
+plt.plot(y_test.index, y_test.target_t1.values, label="test")
+plt.title("Train/Test split")
+plt.xticks(rotation=45)
+plt.show()
+
+
+# In[34]:
+
+
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error
+
+reg = LinearRegression().fit(X_train, y_train["target_t1"])
+p_train = reg.predict(X_train)
+p_test = reg.predict(X_test)
+
+RMSE_train = np.sqrt(mean_squared_error(y_train["target_t1"], p_train))
+RMSE_test = np.sqrt(mean_squared_error(y_test["target_t1"], p_test))
+
+print("Train RMSE: {}\nTest RMSE: {}".format(RMSE_train, RMSE_test) )
+
+
+# In[35]:
+
+
+from sklearn.model_selection import TimeSeriesSplit, ParameterGrid
+
+splits = TimeSeriesSplit(n_splits=3, max_train_size=365*2)
+for train_index, val_index in splits.split(X_train):
+    print("TRAIN:", len(train_index), "TEST:", len(val_index))
+    y_train["target_t1"][train_index].plot()
+    y_train["target_t1"][val_index].plot()
+    plt.show()
+
+
+# In[36]:
+
+
+from sklearn.ensemble import RandomForestRegressor
+
+splits = TimeSeriesSplit(n_splits=3, max_train_size=365*2)
+rfr = RandomForestRegressor()
+# Create a dictionary of hyperparameters to search
+rfr_grid = {"n_estimators": [500], 
+        'max_depth': [3, 5, 10, 20, 30], 
+        'max_features': [4, 8, 16, 32, 59], 
+        'random_state': [123]}
+rfr_paramGrid = ParameterGrid(rfr_grid)
+
+def TimeSplit_ModBuild(model, paramGrid, splits, X, y):
+    from sklearn.model_selection import TimeSeriesSplit
+    from sklearn.metrics import mean_squared_error
+
+    #Loop over each time split and for each
+    for train_index, val_index in splits.split(X_train):
+        _X_train_ = X.iloc[train_index]
+        _y_train_ = y.iloc[train_index]
+        _X_val_ = X.iloc[val_index]
+        _y_val_ = y.iloc[val_index]
+
+        train_scores = []
+        val_scores = []
+        #models = []
+        
+        # Loop through the parameter grid, set the hyperparameters, and save the scores
+        for g in paramGrid:
+            model.set_params(**g)
+            model.fit(_X_train_, _y_train_)
+            p_train = model.predict(_X_train_)
+            p_val = model.predict(_X_val_)
+            score_train = np.mean(mean_squared_error(_y_train_, p_train))
+            score_val = np.mean(mean_squared_error(_y_val_, p_val))
+            train_scores.append(score_train)
+            val_scores.append(score_val)
+            #models.append(model)
+            best_idx = np.argmin(val_scores)
+            
+        print("Best-Fold HyperParams:: ", paramGrid[best_idx])
+        print("Best-Fold Train RMSE: ", train_scores[best_idx])
+        print("Best-Fold Val RMSE: ",val_scores[best_idx])
+        print("\n")
+        
+    #Return most recent model
+    return train_scores, val_scores, best_idx
+
+
+CV_rfr_tup = TimeSplit_ModBuild(rfr, rfr_paramGrid, splits, X_train, y_train["target_t1"])
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
